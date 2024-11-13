04701892c515a0d930eab33db062babe3b85f19d
IVMeer <ott1e3r@gmail.com>
2024-11-13 16:48:32 +0800

    move digits to MINST_study fold

--- Nov/MINST_study/imgProcessing.py
+++ Nov/MINST_study/imgProcessing.py
@@ -1,11 +1,12 @@
 import cv2 as cv
 # for i in range():
 #     pass
-# for i in range(10):
-#     src = cv.imread('E:\\workspace\\python_demo\\digits\\'+str(i)+'.jpg')#读取图片
-#     gray = cv.cvtColor(src, cv.COLOR_BGR2GRAY)
-#     ret, binary = cv.threshold(gray, 0, 255,cv.THRESH_BINARY_INV|cv.THRESH_OTSU)#白底黑字转换为黑底白字
-#     cv.imwrite('E:\\workspace\\python_demo\\digits_processed\\new'+str(i)+'.jpg', binary)#将图像数据写入到图像文件中
+for i in range(10):
+
+    src = cv.imread('E:\\workspace\\python_demo\\MINST_study\\digits\\'+str(i)+'.jpg')#读取图片
+    gray = cv.cvtColor(src, cv.COLOR_BGR2GRAY)
+    ret, binary = cv.threshold(gray, 0, 255,cv.THRESH_BINARY_INV|cv.THRESH_OTSU)#白底黑字转换为黑底白字
+    cv.imwrite('E:\\workspace\\python_demo\\MINST_study\\digits_processed\\new'+str(i)+'.jpg', binary)#将图像数据写入到图像文件中
 
 
 src = cv.imread('E:\\workspace\\python_demo\\digits\\0.jpg')#读取图片
