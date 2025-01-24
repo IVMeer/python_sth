4af626a67c6e2a2836306bdd2cb9e8bd5a1ba7fe
IVMeer <ott1e3r@gmail.com>
2025-01-24 17:07:41 +0800

    update_demo1

--- Dec/Excel/Finished-demo.py
+++ Dec/Excel/Finished-demo.py
@@ -14,14 +14,13 @@
 
 import pandas as pd
 from datetime import datetime
-file_name = r'E:\workspace\python_demo\Dec\Excel\0124-唐山.csv'
+file_name = r'E:\workspace\python_demo\Dec\Excel\0124-tangshan.csv'
 
 # 读取 CSV 文件并转换为 DataFrame
 df = pd.read_csv(file_name)
 
 # 输入日期
 date = '2025/1/24'
-
 # 过滤条件第一列为date所需要的日期且第十列为已完成。
 
 # 第一种过滤方式
@@ -34,18 +33,28 @@ date = '2025/1/24'
 # filter_df = df[(df[date_col] == date) & (df[status_col] == '已完成')]
 
 # 第三种过滤方式
-filter_df = df[(df[df.columns[0]] == date) & (df[df.columns[9]] == '已完成')]
+# filter_df = df[(df[df.columns[0]] == date) & (df[df.columns[9]] == '已完成')]
+
+# 第四种过滤方式
+filter_df = df[(df['日期'] == date) & (df['完成情况'] == '已完成')]
 
 
 # print("first_fileter:",filter_df)
 
 # 处理摄像头编号（第四列）和文件夹编号（第五列），并合并。
+# filter_df['合并结果'] = (
+#     filter_df.iloc[:, 2].astype(int).astype(str) + '-' +  # 数据包日期（第三列）
+#     filter_df.iloc[:, 3].astype(int).astype(str).str.zfill(2) + '-' +  # 摄像头编号（第四列）
+#     # filter_df.iloc[:, 4].astype(int).astype(str)  # 文件夹编号（第五列）
+#     filter_df.iloc[:, 4].apply(lambda x: str(x) if isinstance(x, str) and '_' in x else str(int(x)))  # 文件夹编号（第五列），处理 1 和 1_1 等情况
+# )
+# 
 filter_df['合并结果'] = (
-    filter_df.iloc[:, 2].astype(int).astype(str) + '-' +  # 数据包日期（第三列）
-    filter_df.iloc[:, 3].astype(int).astype(str).str.zfill(2) + '-' +  # 摄像头编号（第四列）
-    # filter_df.iloc[:, 4].astype(int).astype(str)  # 文件夹编号（第五列）
-    filter_df.iloc[:, 4].apply(lambda x: str(x) if isinstance(x, str) and '_' in x else str(int(x)))  # 文件夹编号（第五列），处理 1 和 1_1 等情况
+    filter_df['数据包日期'].astype(int).astype(str) + '-' +
+    filter_df[' 机器号'].astype(int).astype(str).str.zfill(2) + '-' +
+    filter_df['文件夹号数'].apply(lambda x: str(x) if isinstance(x, str) and '_' in x else str(int(x)))
 )
+
 
 # 删除原始的三列（数据包日期、摄像头编号、文件夹编号）
 filter_df.drop(columns=[filter_df.columns[2], filter_df.columns[3], filter_df.columns[4]], inplace=True)
